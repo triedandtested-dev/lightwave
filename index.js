bd1c35e2e64daacf5ee81c4da0073110e856059b
Bryan Dunkley <dunkley.bryan@gmail.com>
2020-09-04 02:22:35 +0100

    ajusted package options

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const LwRFClient = require('./api/client');
+const FirestoreStorage = require('./api/storage');
+
 exports.printMsg = function() {
   console.log('This is a message from the demo package');
 };
+exports.FirestoreStorage = FirestoreStorage;
+exports.LwRFClient = LwRFClient;
